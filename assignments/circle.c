66b593c4b885a62afb69018c6e59e82b1bd7fa91
metinbicaksiz <metinbicaksiz@gmail.com>
2025-03-11 10:44:17 -0700

    loops

--- assignments/circle.c
+++ assignments/circle.c
@@ -10,7 +10,6 @@ int main() {
      */
 
     int radius = 5;
-<<<<<<< HEAD
     float pi = 3.14;
 
     float area = pi * radius * radius;
@@ -18,14 +17,12 @@ int main() {
 
     printf("The area is: %.2f\n", area);
     printf("The perimeter is: %.2f\n", perimeter);
-=======
     const float pi = 3.14;
     float perimeter = 2* pi *radius;
     float area = pi *radius*radius;
   
     printf("The perimeter of the circle = %.3f inches\n", perimeter);
     printf("The areas of the circle     = %.3f inches\n", area);
->>>>>>> 761d188b84fd3068a41e5cd69238c14c84975109
 
     return 0;
 }
